eaa0da0bc7bf4ee52033fa60e2ae077f53944f50
Halina Hrekava <gluk2019@mail.ru>
2021-05-23 18:36:02 +0300

    Junit4 added

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 import org.junit.Test;
 
-import static org.junit.Assert. assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashBackHackerTest {
     CashBackHacker service = new CashBackHacker();
